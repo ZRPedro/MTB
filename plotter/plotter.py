6cb203f184f0b57f270419cb5aeff0c94d703607
Peter Jan Randewijk <prw@energinet.dk>
2025-07-23 08:45:42 +0200

    Fix table header width calculation in HTML plots for improved layout accuracy

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -616,7 +616,7 @@ def create_html_plots(columns, plots, title):
     html_content += '<table style="width:100%">'
     html_content += '<tr>'
     for i in range(columns):
-        html_content += f'<th style="width:{100/columns}%"> &nbsp; </th>'
+        html_content += f'<th style="width:{round(100/columns)}%"> &nbsp; </th>'
     html_content += '</tr>'
     for i, p in enumerate(plots):
         plot_title: str = p['layout']['title']['text']  # type: ignore
