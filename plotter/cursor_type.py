eb99449a9b22bd3efad941f9a91fcd59a3845507
Peter Jan Randewijk <prw@energinet.dk>
2025-07-29 13:57:38 +0200

    The CursorType enum was expanded to include many more cursor metric functions

--- plotter/cursor_type.py
+++ plotter/cursor_type.py
@@ -1,8 +1,19 @@
 from enum import Enum
 
 class CursorType(Enum):
-    MIN_MAX = 1
-    AVERAGE = 2
+    MIN        = 1
+    MAX        = 2
+    MEAN       = 3    
+    GRAD_MIN   = 4
+    GRAD_MAX   = 5
+    GRAD_MEAN  = 6
+    RESPONSE   = 7
+    RISE_FALL  = 8
+    SETTLING   = 9
+    OVERSHOOT  = 10
+    FSM_SLOPE  = 11
+    LFSM_SLOPE = 12
+    QU_SLOPE   = 13
 
     @classmethod
     def from_string(cls, string : str):
