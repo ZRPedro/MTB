368d30295a7b91a8ecc9fa6a73f89fb69b68e031
Peter Jan Randewijk <prw@energinet.dk>
2025-07-30 13:23:15 +0200

    Enhance genIdealResults function to include delay and low-pass filtering for ideal signals; update return structure to include new signal columns.

--- plotter/ideal_functions.py
+++ plotter/ideal_functions.py
@@ -49,30 +49,30 @@ def genIdealResults(result, resultData, settingDict, caseDf, pscadInitTime):
             
             # Only apply from t >= 0
             mask = idealData['time'] >= 0            
-            newPpocSeries = pd.Series((idealLFSM(Pref=P0, f=f, DK=DK, FSM=FSM, s_fsm=s_fsm, db=db) for f in idealData.loc[mask, 'pll_f_hz']),
+            newPpocSeries = pd.Series([idealLFSM(Pref=P0, f=f, DK=DK, FSM=FSM, s_fsm=s_fsm, db=db) for f in idealData.loc[mask, 'pll_f_hz']],
                                        index=idealData.loc[mask].index) 
             idealData.loc[mask, 'P_pu_PoC'] = newPpocSeries
             
             # Adding delay and LP filtering 
-            #Ts = idealData.time.iloc[1]-idealData.time.iloc[0]
-            #Td = 0.5            # delay time [s]
-            #trise = 1           # rise time [s]
-            #fc = 0.35/trise     # cut off frequency [Hz]
-            #idealData.P_pu_PoC_Td = delay(idealData.P_pu_PoC, Td, Ts)
-            #idealData.P_pu_PoC_Td_LPF = lpf(idealData.P_pu_PoC, fc, 1/Ts)
+            Ts = idealData.time.iloc[1]-idealData.time.iloc[0]
+            Td = 0.5            # delay time [s]
+            trise = 1           # rise time [s]
+            fc = 0.35/trise     # cut off frequency [Hz]
+            idealData['P_pu_PoC_Td'] = delay(idealData.P_pu_PoC, Td, Ts)        # Add new column for the delayed ideal signal
+            idealData['P_pu_PoC_Td_LPF'] = lpf(idealData.P_pu_PoC, fc, 1/Ts)    # Add new column for the filtered ideal signal
             
-            #returnDict = {'figs': ['Ppoc', 'Ppoc', 'Ppoc'], 'signals': ['P_pu_PoC', 'P_pu_PoC_Td', 'P_pu_PoC_Td_LPF'], 'data': idealData}
-            returnDict = {'figs': ['Ppoc'], 'signals': ['P_pu_PoC'], 'data': idealData}
+            returnDict = {'figs': ['Ppoc', 'Ppoc', 'Ppoc'], 'signals': ['P_pu_PoC', 'P_pu_PoC_Td', 'P_pu_PoC_Td_LPF'], 'data': idealData}
+            #returnDict = {'figs': ['Ppoc'], 'signals': ['P_pu_PoC'], 'data': idealData}
             
         # Q(U) control cases
         elif 'Ucontrol' in caseDf['Case']['Name'].squeeze():
-            U0 = caseDf['Initial Settings']['U0'].squeeze()                     # Initial U0
+            U0 = caseDf['Initial Settings']['U0'].squeeze()                     # Initial voltage reference, U0
             s_V_droop = settingDict['V droop']
             Qref0 = 0
             
             # Only apply from t >= 0
             mask = idealData['time'] >= 0            
-            newQpocSeries = pd.Series((idealQU(Qref=Qref0, Uref=U0, uag=uag, s=s_V_droop) for uag in idealData.loc[mask,'meas_Vag_pu']),
+            newQpocSeries = pd.Series([idealQU(Qref=Qref0, Uref=U0, uag=uag, s=s_V_droop) for uag in idealData.loc[mask,'meas_Vag_pu']],
                                        index=idealData.loc[mask].index)            
             idealData.loc[mask, 'Q_pu_PoC'] = newQpocSeries
             
@@ -80,14 +80,14 @@ def genIdealResults(result, resultData, settingDict, caseDf, pscadInitTime):
         
         #PF control mode
         elif 'Qpf' in caseDf['Case']['Name'].squeeze():
-            P0 = caseDf['Initial Settings']['P0'].squeeze()                     # Initial active power setpoint
+            P0 = caseDf['Initial Settings']['P0'].squeeze()                     # Initial active power setpoint, P0
             assert caseDf['Initial Settings']['Qmode'].squeeze()  == 'PF'
-            Qref0 = caseDf['Initial Settings']['Qref0'].squeeze()               # Initial PF setpoint
+            Qref0 = caseDf['Initial Settings']['Qref0'].squeeze()               # Initial PF setpoint, Qref0, when Qmode == 'PF'
             
             if caseDf['Event 1']['type'].squeeze() == 'Pref':   # Pref changes -> slow ramping of Ppoc, thus use Ppoc and not Pref
                 idealData.Q_pu_PoC = idealQpf(idealData.P_pu_PoC, Qref0)
             elif caseDf['Event 1']['type'].squeeze() == 'Qref': # PFref changes & Pref constant
-                idealData.Q_pu_PoC = idealQpf(idealData.mtb_s_pref_pu, idealData.mtb_s_qref)
+                idealData.Q_pu_PoC = idealQpf(idealData.mtb_s_pref_pu, idealData.mtb_s_qref) # Note that .mtb_s_qref = PF if Qmode == 'PF'
                 
             returnDict = {'figs': ['Qpoc'], 'signals': ['Q_pu_PoC'], 'data': idealData}
             
@@ -114,34 +114,64 @@ def genIdealResults(result, resultData, settingDict, caseDf, pscadInitTime):
 
     return returnDict
 
-def idealQpf(Ppoc, PFref): # Note that PF = Qref0 if Qmode == 'PF'
-    theta = np.arccos(PFref)
-    Qpoc = Ppoc/np.tan(theta)
-    
-    return Qpoc
-    
 
-# Simple first ordedr Low Pass Filter
 def lpf(x, fc, fs):
+    '''
+    Simple first order low pass filter with cut off frequency fc and sampling frequency fs
+
+    Parameters:
+        x: input signal 
+        fc: cut off frequency in Hz
+        fs: sampling frequency in Hz
+    
+    Returns:
+        filtered signal 
+    '''
     wc = 2*np.pi*fc
     b, a = bilinear([0, wc], [1, wc], fs)
+
     return lfilter(b, a, x)
 
-# Simple signal delay
+
 def delay(x, Td, Ts):
+    '''
+    Simple signal delay of Td seconds with sampling time Ts
+
+    Parameters:
+        x: input signal
+        Td: delay time in seconds
+        Ts: sampling time in seconds
+
+    Returns:
+        delayed signal
+    '''
     delay_samples = int(round(Td/Ts))
     b = np.zeros(delay_samples + 1)
     b[delay_samples] = 1    # For a pure delay of N samples, b = [0, 0, ..., 0, 1] (where 1 is at index N)
     a = np.array([1.0])     # For an FIR filter (pure delay), a = [1]
+
+    if delay_samples == 0:
+        return x  # No delay needed, return original signal
+    
+    # Apply the filter to the input signal
     return lfilter(b, a, x)
 
-# Ideal Active Power Ramping function
+
 def idealPramp(Pref, Tstep, Pstep, t):
     '''
-    With Pref in pu, the function calculates the maximum limits on rates of 
-    change of active power output (ramping limits) in both an up and down 
-    direction of change of active power output for a power-generating module
+    This function calculates the ideal or maximum on rates of change of 
+    active power output (Pramp) in both an up and down direction of for 
+    a change in the active power reference for power-generating modules
     based on RfG (EU) 2016/631, 15.6 (e) NC 2025 (Version 4)
+
+    Parameters:
+        Pref in [pu] -- for Power Park Modules, Pref is the actual Active Power output 
+        Tstep in [s] -- time step for the ramping
+        Pstep in [pu] -- the new value of P after the ramping   
+        t in [s] -- time at which the new value of P is calculated
+
+    Returns:
+        Pramp in [pu] -- the new value of P after the ramping
     '''
     if Pstep > Pref:
         m =  0.0033   # pu/s - equivalent to 0.2 pu/min
@@ -154,23 +184,31 @@ def idealPramp(Pref, Tstep, Pstep, t):
         
     return Pramp
 
-# Ideal LFSM function
+
 def idealLFSM(Pref, f, DK=1, FSM=False, s_fsm=10, db=0):
     '''
-    With Pref in pu, the function calculates the new value of P given the
+    This function calculates the new value of P given the
     frequency f, according to RfG (EU) 2016/631, 13.2 (a-d) and NC 2025
-    (Version 4)for either DK1 or DK2
-    * Pref in [pu] -- for Power Park Modules, Pref is the actual Active Power 
-      output and not the nominal power
-    * LFSM droop, s in [%]
-    
-    If f > fn (50Hz),
-        the output will be the LFSM-O value
-    else if f < fn (50Hz),
-        it will be the LFSM-U value
-    If FSM is True,
-        the FSM droop, "s_fsm" has to be specified 
-        as well as the deadband, "db" if any
+    (Version 4) for either DK1 or DK2
+
+    Parameters:
+        Pref in [pu] -- for Power Park Modules, Pref is the actual Active Power output     
+        f in [Hz] -- the actual frequency at the point of connection
+        DK in [1,2] -- the Danish area, either 1 or 2
+        FSM in [True, False] -- if True, the function will use the FSM droop and deadband
+        s_fsm in [%] -- the FSM droop in [%]
+        db in [Hz] -- the FSM deadband in [Hz]
+
+        If f > fn (50Hz),
+            the output will be the LFSM-O value
+        else if f < fn (50Hz),
+            it will be the LFSM-U value
+        If FSM is True,
+            the FSM droop, "s_fsm" has to be specified 
+            as well as the deadband, "db" if any
+
+    Returns:
+        Pnew in [pu] -- the new value of P after the frequency change
     '''
     Pn = 1.0 #pu
     fn = 50  #Hz
@@ -197,16 +235,22 @@ def idealLFSM(Pref, f, DK=1, FSM=False, s_fsm=10, db=0):
         
     return Pnew
 
-# Ideal FSM function
+
 def idealFSM(Pref, f, DK=1, s=10, db=0):
     '''
     With Pref in pu, the function calculates the new value of P given the
     frequency f, for the FSM droop, s, according to RfG (EU) 2016/631, 15.2 (d)
     and NC 2025 (Version 4)
-    * Pref in [pu] -- for Power Park Modules, Pref is the actual Active Power
-      output and not the nominal power
-    * FSM droop, s in [%]
-    * FSM deadband, db in [Hz]
+
+    Parameters:
+        Pref in [pu] -- for Power Park Modules, Pref is the actual Active Power output and not the nominal power
+        f in [Hz] -- the actual frequency at the point of connection
+        DK in [1,2] -- the Danish area, either 1 or 2
+        s in [%] -- the FSM droop in [%]
+        db in [Hz] -- the FSM deadband in [Hz]
+
+    Returns:
+        Pnew in [pu] -- the new value of P after the frequency change 
     '''
     Pn = 1.0 #pu
     fn = 50  #Hz
@@ -230,11 +274,20 @@ def idealFSM(Pref, f, DK=1, s=10, db=0):
     
     return Pnew
 
-# Ideal Q(U) control function
+
 def idealQU(Qref, Uref, uag, s):
     '''
-    This function calculates the reactive power required for voltage control
+    This function calculates the value of the reactive power required for voltage control
     mode based on RfG (EU) 2016/631, 21.3 (d) NC 2025 (Version 4)
+
+    Parameters:
+        Qref in [pu] -- Qref is the actual Reactive Power output and not the nominal reactive power value
+        Uref in [pu] -- Uref is the actual voltage reference at the point of connection
+        uag in [pu] -- the measured phase to ground voltage at the point of connection    
+        s in [%] -- the voltage droop of the Q(U) control   
+
+    Returns:
+        Qnew in [pu] -- the new value of Q at the point of connection
     '''
     Qnom = 0.33
     du = Uref-uag
@@ -243,12 +296,37 @@ def idealQU(Qref, Uref, uag, s):
         
     return Qnew
 
-# Ideal Fast Fault Current contribution function
+
+def idealQpf(Ppoc, PFref): 
+    '''
+    This function calculates the ideal reactive power output (Qpoc) based on the active power output (Ppoc)
+    and the power factor reference (PFref) according to RfG (EU) 2016/631, 21.3 (d) NC 2025 (Version 4)
+
+    Parameters:
+        Ppoc in [pu] -- the actual Active Power output at the point of connection
+        PFref in [pu] -- the power factor reference
+
+    Returns:
+        Qpoc in [pu] -- the new value of Q at the point of connection
+    '''
+    theta = np.arccos(PFref)
+    Qpoc = np.clip(Ppoc*np.tan(theta), -0.33, 0.33)  # Ensure Qpoc is within [-0.33, 0.33] range
+                    
+    return Qpoc
+    
+
 def idealFFC(vpos_val, id_val, iq_val, DK=1 , DSO=False):
     '''
     This function calculates the fast fault currunt (FFC) contribution,
     id_ffc and iq_ffc, based on the positive sequence voltage magnitude,
     based on RfG (EU) 2016/631, 20.2 (b) NC 2025 (Version 4) for DK1 and DK2
+    * vpos_val in [pu] -- the positive sequence voltage magnitude at the point of connection
+    * id_val in [pu] -- the actual positive sequence direct current at the point of connection
+    * iq_val in [pu] -- the actual positive sequence quadrature current at the point of connection
+    * DK in [1,2] -- the Danish area, either 1 or 2
+    * DSO in [True, False] -- if True, the function will use the DSO voltage limit of 0.9 pu, otherwise it will use the TSO voltage limit of 0.85 pu
+    Returns:
+        tuple of (id_ffc, iq_ffc) in [pu]
     '''
     if DK == 1:
         vposFrtLimit = 0.85
